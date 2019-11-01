5f0d7db61206c84a2ce36ca42cbf6fd591fa6834
Paola Castro <patriciacastrofernandez0715@gmail.com>
2019-11-01 18:24:27 -0500

    C:/Program Files/Git/

--- prueba2.php
+++ prueba2.php
@@ -12,27 +12,29 @@ while($reg = $result->fetchObject()){
         $pdf = new FPDF();
         $pdf->AddPage();
         $pdf->SetFont('Arial', 'B', 16);
-        $pdf->Cell(130, 50,'IMAGE', 1);
-        $pdf->Ln(50);
+        //Ver en que formato esta la imagen PNG,JPG
+        //$pdf->Cell(130, 50,$pdf->Image($reg->href, $pdf->GetX(), $pdf->GetY(),0), 1);
+        $pdf->Cell(100, 60,$pdf->Image($reg->href,20, 10,0), 1);
+        $pdf->Ln(60);
         $pdf->SetFont('Arial', 'I', 12);
         $pdf->Cell(50, 10, 'Nombres', 1);
-        $pdf->Cell(80, 10,$reg->nombres, 1);
+        $pdf->Cell(50, 10,$reg->nombres, 1);
         $pdf->Ln(10);
         $pdf->SetFont('Arial', 'I', 12);
         $pdf->Cell(50, 10, 'Apellido Paterno', 1);
-        $pdf->Cell(80, 10,$reg->apellidopaterno, 1);
+        $pdf->Cell(50, 10,$reg->apellidopaterno, 1);
         $pdf->Ln(10);
         $pdf->SetFont('Arial', 'I', 12);
         $pdf->Cell(50, 10, 'Apellido Materno', 1);
-        $pdf->Cell(80, 10, $reg->apellidomaterno, 1);
+        $pdf->Cell(50, 10, $reg->apellidomaterno, 1);
         $pdf->Ln(10);
         $pdf->SetFont('Arial', 'I', 12);
         $pdf->Cell(50, 10, 'Escuela Pro.', 1);
-        $pdf->Cell(80, 10, $reg->escuela, 1);
+        $pdf->Cell(50, 10, $reg->escuela, 1);
         $pdf->Ln(10);
         $pdf->SetFont('Arial', 'I', 12);
         $pdf->Cell(50, 10, 'Universidad', 1);
-        $pdf->Cell(80, 10, $reg->universidad, 1);
+        $pdf->Cell(50, 10, $reg->universidad, 1);
         $pdf->Output(); 
 }
 ?>
--- registro.php
+++ registro.php
@@ -7,17 +7,43 @@ $dni = $_POST['txtdni'];
 $universidad = $_POST['cbouniversidad'];
 $escuela = $_POST['cboescuela'];
 
-$sql="INSERT INTO asistente (dni,nombres,apellidomaterno,apellidopaterno,escuela,universidad)
-VALUES ('$dni','$nom','$apeM','$apeP','$escuela','$universidad')";
-$resp=1;
-$cnx->query($sql) or $resp=0;
-echo $resp;
+//Subir la Imagen
+//Creamos una variable para ver si se sube o no el archivo
+$imgload="true";
 
-session_start();
-$_SESSION['dni']= $dni;
+//Seteamos nombre, tipo y tamaño del archivo
+$file_name=$_FILES['img']['name'];
+$img_size=$_FILES['img']['size'];
+$file_type=$_FILES['img']['type'];
 
-header("location: prueba2.php");
+//verificamos tamaño
+if ($img_size>200000){
+    $imgload="false";
+}
+//verificamos que solo sea imagen
+if (!($file_type =="image/jpeg" OR $file_type=="image/gif")){
+    // Tu archivo tiene que ser JPG o GIF. Otros archivos no son permitidos<BR>";
+    $imgload="false";
+}
+//seteamos la ruta de la carpeta
+$add="uploads/$file_name";
+//lo movemos del temporal a la carpeta
+if($imgload=="true"){
+    move_uploaded_file ($_FILES['img']['tmp_name'], $add);
 
+    $sql="INSERT INTO asistente (dni,nombres,apellidomaterno,apellidopaterno,escuela,universidad,href)
+    VALUES ('$dni','$nom','$apeM','$apeP','$escuela','$universidad','$add')";
+    $resp=1;
+    $cnx->query($sql) or $resp=0;
+    echo $resp;
+    if ($resp =1 ){
+        session_start();
+        $_SESSION['dni']= $dni;
+        header("location: prueba2.php");
+    }else{
+        header("location: index.html");
+    }
+}
 ?>
 
 
